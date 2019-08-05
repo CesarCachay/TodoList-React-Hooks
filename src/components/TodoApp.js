35a81ec5815c82a6ab2c9560227321fc48d03138
Cesar Cachay <cesarcachay1996@gmail.com>
2019-08-04 23:22:42 -0500

    Add new todo after submit

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,10 +2,12 @@
 import React from "react";
 import { jsx } from "@emotion/core";
 
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
-import TodoList from "./TodoList";
 
 const paperHeight = {
   padding: "0px",
@@ -23,13 +25,17 @@ function TodoApp() {
 
   const [todos, setTodos] = React.useState(initialTodos);
 
+  function addTodo(newTodo) {
+    setTodos([...todos, { id: 4, task: newTodo, completed: false }]);
+  }
+
   return (
     <Paper style={paperHeight} elevation={0}>
       <AppBar color="primary" position="static" style={{ height: "64px" }}>
         <Typography color="inherit">TODOS WITH HOOKS</Typography>
       </AppBar>
+      <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} />
-      <TodoForm />
     </Paper>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,10 +3,22 @@ import React from "react";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 
-function TodoForm() {
+import useInputState from "../hooks/useInputState";
+
+function TodoForm({ addTodo }) {
+  const [value, handleChange, reset] = useInputState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    addTodo(value);
+    reset();
+  }
+
   return (
     <Paper>
-      <TextField />
+      <form onSubmit={handleSubmit}>
+        <TextField value={value} onChange={handleChange} />
+      </form>
     </Paper>
   );
 }
