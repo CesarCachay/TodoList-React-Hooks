5a7d6b7b1aeafa74619ff02275dd3bc727444883
Cesar Cachay <cesarcachay1996@gmail.com>
2019-08-04 23:57:28 -0500

    Add styles for Todo component & improvement of accessibility

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,16 +4,24 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
 function Todo({ task, completed }) {
   return (
     <ListItem>
       <Checkbox tabIndex={-1} checked={completed} />
-      <ListItemText>{task}</ListItemText>
+      <ListItemText
+        style={{ textDecoration: completed ? "line-through" : "none" }}
+      >
+        {task}
+      </ListItemText>
       <ListItemSecondaryAction>
         <IconButton>
           <DeleteIcon />
+        </IconButton>
+        <IconButton>
+          <EditIcon />
         </IconButton>
       </ListItemSecondaryAction>
     </ListItem>
